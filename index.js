44315b5ade8e14ac277ad746e1e1b72ebf5d404c
JavedAhmadDeveloper <javed.ahmad@mergestack.com>
2022-08-18 14:43:15 +0500

    get request added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.get("/students", (req, res) => {
+  res.status(200).send({
+    name: "Javed Ahmad",
+    company: "MergeStack",
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`its live on adress http://localhost:${PORT}`)
